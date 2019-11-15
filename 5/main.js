45148420f369fb55e603ab061c03405137d5d34a
mycalhunter <mycalhunter@gmail.com>
2019-11-15 14:53:17 -0500

    change backgroundcolor if checked

--- 5/main.js
+++ 5/main.js
@@ -1,6 +1,12 @@
-document.addEventListener("click", function(e){
-  var checkboxs = document.querySelectorAll(".checkbox");
-  for (i = 0; i > checkboxs.length; i++){
-    console.log(checkboxs[i]);
+var checkboxs = document.getElementsByClassName("checkbox");
+document.addEventListener("click", function() {
+  for (i = 0; i < checkboxs.length; i++) {
+    if (checkboxs[i].checked) {
+      //console.log("Checked");
+      checkboxs[i].parentElement.style.backgroundColor = "#ff923be8";
+      //console.log(checkboxs[i].parentElement);
+    } else {
+      checkboxs[i].parentElement.style.backgroundColor = "#3ec2b5ed";
+    }
   }
 });
