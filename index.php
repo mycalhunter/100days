93781e23f27c2c97e4d5880187d0d023e5d550c4
Hunter Miller <mycalhunter@gmail.com>
2019-11-21 14:34:53 -0500

    change index.php to use JSON

--- index.php
+++ index.php
@@ -1,206 +1,44 @@
-<?php
-$gitSlug = "//github.com/mycalhunter/100days/tree/master/";
-$gitLogo = "//pngimg.com/uploads/github/github_PNG15.png";
-$viewSlug = "/100days/";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>100 Days of Code | iDevHunter</title>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"> </script>
   <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
 </head>
 <body>
   <div class="title">
     <h1>100 Days of Code</h1>
   </div>
-  
-  <div class="days">
-    <div class="day">
-      <h2>Day 1</h2>
-        <img src="/100days/images/day_1.png">
-      <div class="techs">
-        <p class="tech">HTML</p>
-        <p class="tech">CSS</p>
-        <p class="tech">Javascript</p>
-        <p class="tech">jQuery</p>
-      </div>
-      <div class="viewBtn view">
-        <a href="<? echo $viewSlug . "1" . "/" ?>" target="_blank">View</a>
-      </div>
-      <div class="viewBtn">
-        <a href="<? echo $gitSlug . '1' ?>" target="_blank">
-          <img src="<? echo $gitLogo ?>">
-        </a>
-      </div>
-    </div>
-    
-    
-    <div class="day">
-      <h2>Day 2</h2>
-        <img src="/100days/images/day_2.png">
-      <div class="techs">
-        <p class="tech">HTML</p>
-        <p class="tech">CSS</p>
-        <p class="tech">jQuery</p>
-        <p class="tech">PHP</p>
-        <p class="tech">MySQLi</p>
-      </div>
-      <div class="viewBtn view">
-        <a href="<? echo $viewSlug . "2" . "/" ?>" target="_blank">View</a>
-      </div>
-      <div class="viewBtn">
-        <a href="<? echo $gitSlug . '2' ?>" target="_blank">
-          <img src="<? echo $gitLogo ?>">
-        </a>
-      </div>
-    </div>
-    
-    
-    <div class="day">
-      <h2>Day 3</h2>
-        <img src="/100days/images/day_3.png">
-      <div class="techs">
-        <p class="tech">HTML</p>
-        <p class="tech">CSS</p>
-        <p class="tech">jQuery</p>
-      </div>
-      <div class="viewBtn view">
-        <a href="<? echo $viewSlug . "3" . "/" ?>" target="_blank">View</a>
-      </div>
-      <div class="viewBtn">
-        <a href="<? echo $gitSlug . '3' ?>" target="_blank">
-          <img src="<? echo $gitLogo ?>">
-        </a>
-      </div>
-    </div>
-    
-    <div class="day">
-      <h2>Day 4</h2>
-        <img src="/100days/images/day_4.png">
-      <div class="techs">
-        <p class="tech">PHP</p>
-        <p class="tech">MySQLi</p>
-      </div>
-      <div class="viewBtn view">
-        <a href="<? echo $viewSlug . "4" . "/" ?>select.php" target="_blank">View</a>
-      </div>
-      <div class="viewBtn">
-        <a href="<? echo $gitSlug . '4' ?>" target="_blank">
-          <img src="<? echo $gitLogo ?>">
-        </a>
-      </div>
-    </div>
-    
-    <div class="day">
-      <h2>Day 5</h2>
-        <img src="/100days/images/day_5.png">
-      <div class="techs">
-        <p class="tech">HTML</p>
-        <p class="tech">CSS</p>
-        <p class="tech">Javascript</p>
-        <p class="tech">PHP</p>
-        <p class="tech">MySQLi</p>
-      </div>
-      <div class="viewBtn view">
-        <a href="<? echo $viewSlug . "5" . "/" ?>" target="_blank">View</a>
-      </div>
-      <div class="viewBtn">
-        <a href="<? echo $gitSlug . '5' ?>" target="_blank">
-          <img src="<? echo $gitLogo ?>">
-        </a>
-      </div>
-    </div>
-    
-    <div class="day">
-      <h2>Day 6</h2>
-        <img src="/100days/images/day_6.png">
-      <div class="techs">
-        <p class="tech">HTML</p>
-        <p class="tech">CSS</p>
-        <p class="tech">PHP</p>
-      </div>
-      <div class="viewBtn view">
-        <a href="<? echo $viewSlug . "6" . "/" ?>" target="_blank">View</a>
-      </div>
-      <div class="viewBtn">
-        <a href="<? echo $gitSlug . '6' ?>" target="_blank">
-          <img src="<? echo $gitLogo ?>">
-        </a>
-      </div>
-    </div>
-    
-    <div class="day">
-      <h2>Day 7</h2>
-        <img src="/100days/images/day_7.png">
-      <div class="techs">
-        <p class="tech">HTML</p>
-        <p class="tech">CSS</p>
-        <p class="tech">PHP</p>
-        <p class="tech">Javascript</p>
-        <p class="tech">API</p>
-      </div>
-      <div class="viewBtn view">
-        <a href="<? echo $viewSlug . "7" . "/" ?>" target="_blank">View</a>
-      </div>
-      <div class="viewBtn">
-        <a href="<? echo $gitSlug . '7' ?>" target="_blank">
-          <img src="<? echo $gitLogo ?>">
-        </a>
-      </div>
-    </div>
-    
-    <div class="day">
-      <h2>Day 8</h2>
-        <img src="/100days/images/day_8.png">
-      <div class="techs">
-        <p class="tech">HTML</p>
-        <p class="tech">CSS</p>
-        <p class="tech">Javascript</p>
-        <p class="tech">jQuery</p>
-        <p class="tech">API</p>
-      </div>
-      <div class="viewBtn view">
-        <a href="<? echo $viewSlug . "8" . "/" ?>" target="_blank">View</a>
-      </div>
-      <div class="viewBtn">
-        <a href="<? echo $gitSlug . '8' ?>" target="_blank">
-          <img src="<? echo $gitLogo ?>">
-        </a>
-      </div>
-    </div>
-    
-    <div class="day">
-      <h2>Day 9</h2>
-        <img src="/100days/images/day_9.png">
-      <div class="techs">
-        <p class="tech">HTML</p>
-        <p class="tech">CSS</p>
-        <p class="tech">Javascript</p>
-      </div>
-      <div class="viewBtn view">
-        <a href="<? echo $viewSlug . "9" . "/" ?>" target="_blank">View</a>
-      </div>
-      <div class="viewBtn">
-        <a href="<? echo $gitSlug . '9' ?>" target="_blank">
-          <img src="<? echo $gitLogo ?>">
-        </a>
-      </div>
-    </div>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  <div class="days">    
+    <script>
+      $(function() {
+        var days = [];    
+        $.getJSON('days.json', function(data) {
+          $.each(data, function(i, f) {
+            for (i = 0; i < f.length; i++) {
+              var day = `<div class="day"> 
+                          <h2>Day ${f[i].day} </h2> 
+                          <img src="${f[i].image}"> 
+                          <div class="techs"> 
+                            <p class="tech">${f[i].techs}</p> 
+                          </div> 
+                          <div class="viewBtn view"> 
+                            <a href="${f[i].view}${f[i].day}/" target="_blank">View</a> 
+                          </div> 
+                          <div class="viewBtn"> 
+                            <a href="${f[i].gitHub}${f[i].day}" target="_blank"> 
+                              <img src="//pngimg.com/uploads/github/github_PNG15.png"> 
+                            </a> 
+                          </div>
+                        </div>`
+                        $(day).appendTo(".days");
+            }
+          });
+        });
+      });
+    </script>    
   </div>
-  <script src="main.js" type="text/javascript"></script>
 </body>
 </html>
